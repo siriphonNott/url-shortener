cf0ab2670deec273c5dd44ca6aec341a89f861fc
Attitude <developer_03@wrfront.com>
2026-06-19 21:28:55 +0700

    chore(api): remove legacy Express src tree (superseded by Worker; eliminates .js/.ts import collisions)

--- api/src/models/Role.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, trim: true },
-  description: { type: String, default: '' },
-  permissions: { type: mongoose.Schema.Types.Mixed, default: {} },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Role', roleSchema);
--- api/src/routes/redirect.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { redirect } = require('../controllers/redirectController');
-
-router.get('/:code', redirect);
-
-module.exports = router;
